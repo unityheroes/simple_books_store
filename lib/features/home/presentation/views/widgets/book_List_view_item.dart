1bdd3041a5669135d2c3bdae0a11bca47671088d
Ahmed Elhabak <ahmed.elhabak101@gmail.com>
2024-06-01 07:18:52 +0300

    add book list view to home

---
+++ lib/features/home/presentation/views/widgets/book_List_view_item.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class BookListViewItem extends StatelessWidget {
+  const BookListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 3 / 4,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          image: const DecorationImage(
+            image: NetworkImage(
+                'https://images.unsplash.com/photo-1641154748135-8032a61a3f80?q=80&w=1915&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+            fit: BoxFit.fill,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,16 +1,39 @@
 import 'package:flutter/material.dart';
 
 import 'package:simple_books_store/features/home/presentation/views/widgets/custom_app_bar.dart';
+import 'package:simple_books_store/features/home/presentation/views/widgets/book_List_view_item.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return const Column(
       children: [
         CustomAppBar(),
+        BooksListView(),
       ],
+    );
+  }
+}
+
+class BooksListView extends StatelessWidget {
+  const BooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.25,
+      child: ListView.builder(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        itemCount: 10,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+              child: const BookListViewItem());
+        },
+      ),
     );
   }
 }
