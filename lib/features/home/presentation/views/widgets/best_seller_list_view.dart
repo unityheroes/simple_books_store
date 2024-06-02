27f2ad7377bce666b9283a63fb64bffd95ea48e0
Ahmed Elhabak <ahmed.elhabak101@gmail.com>
2024-06-02 03:42:01 +0300

    finish best seller list view and home screen

---
+++ lib/features/home/presentation/views/widgets/best_seller_list_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:simple_books_store/features/home/presentation/views/widgets/best_seller_book_item.dart';
+
+class BestSellerListView extends StatelessWidget {
+  const BestSellerListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      physics: const NeverScrollableScrollPhysics(),
+      padding: EdgeInsets.zero,
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return const Padding(
+          padding: EdgeInsets.symmetric(vertical: 8.0),
+          child: BestSellerBookItem(),
+        );
+      },
+    );
+  }
+}
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:simple_books_store/core/utils/styles.dart';
-import 'package:simple_books_store/features/home/presentation/views/widgets/best_seller_book_item.dart';
+import 'package:simple_books_store/features/home/presentation/views/widgets/best_seller_list_view.dart';
 import 'package:simple_books_store/features/home/presentation/views/widgets/books_list_view.dart';
 import 'package:simple_books_store/features/home/presentation/views/widgets/custom_app_bar.dart';
 
@@ -9,28 +9,38 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const CustomAppBar(),
-          const BooksListView(),
-          Divider(
-            height: MediaQuery.of(context).size.height * .05,
-            thickness: 1,
-            endIndent: (MediaQuery.of(context).size.width / 2) * .5,
-            indent: (MediaQuery.of(context).size.width / 2) * .5,
-          ),
-          const SizedBox(height: 20),
-          const Text(
-            'Best Seller',
-            style: Styles.textStyle18,
-          ),
-          const SizedBox(height: 20),
-          const BestSellerBookItem(),
-        ],
+    return CustomScrollView(slivers: [
+      SliverToBoxAdapter(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 32),
+                child: CustomAppBar()),
+            const BooksListView(),
+            Divider(
+              height: MediaQuery.of(context).size.height * .05,
+              thickness: 1,
+              endIndent: (MediaQuery.of(context).size.width / 2) * .5,
+              indent: (MediaQuery.of(context).size.width / 2) * .5,
+            ),
+            const SizedBox(height: 20),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 32),
+              child: Text(
+                'Best Seller',
+                style: Styles.textStyle18,
+              ),
+            ),
+            const SizedBox(height: 20),
+          ],
+        ),
       ),
-    );
+      const SliverFillRemaining(
+        child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 32),
+            child: BestSellerListView()),
+      )
+    ]);
   }
 }
